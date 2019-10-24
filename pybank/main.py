a89239c05d24c026afe817198ca4a413fa838f7e
Lourdes <lourdes.rm10@gmail.com>
2019-10-23 19:12:04 -0500

    append row (0) to list and print length of the list

--- pybank/main.py
+++ pybank/main.py
@@ -9,10 +9,12 @@ import csv
 
 pybank = os.path.join("budget_data.csv")
 
+months_list=[]
 # pybank = open("budget_data.csv")
 
 with open(pybank, "r", newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
 
     # Read and store header
     csv_header = next(csvreader)
@@ -21,7 +23,9 @@ with open(pybank, "r", newline="") as csvfile:
     #Read through each row of data after the header
     for row in csvreader:
 
-        print(row)
-
-    
-
+        rows=[row[0]]
+        
+        months_list.append(rows)
+        
+        
+print(len(months_list))
