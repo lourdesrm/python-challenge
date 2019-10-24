4f3809edd02c44489ea3e3ff1e46095b1f8a6df4
Lourdes <lourdes.rm10@gmail.com>
2019-10-23 21:05:01 -0500

    append row (1) to calculate total profit/losses and print

--- pybank/main.py
+++ pybank/main.py
@@ -6,10 +6,12 @@
 
 import os
 import csv
+import math as m
 
 pybank = os.path.join("budget_data.csv")
 
 months_list=[]
+total_list=[]
 # pybank = open("budget_data.csv")
 
 with open(pybank, "r", newline="") as csvfile:
@@ -20,12 +22,31 @@ with open(pybank, "r", newline="") as csvfile:
     csv_header = next(csvreader)
     #print(f"Header: {csv_header}")
 
-    #Read through each row of data after the header
+    #Read through each row of data after the header to get the months
     for row in csvreader:
 
         rows=[row[0]]
         
         months_list.append(rows)
+
+        total=[row[1]]
+
+        total_list.append(total)
         
-        
-print(len(months_list))
+    # print total number of months in the dataset   
+print(f"The total number of months in the dataset is {len(months_list)}")
+
+
+flattened=[]
+
+for sublist in total_list:
+    for val in sublist:
+        flattened.append(val)
+
+
+flattened=list(map(int,flattened))
+
+
+# print net total amount of "Profit/Losses"
+print(f"The net total amount of Profit/Losses in the dataset is equal to {m.fsum(flattened)}")
+
