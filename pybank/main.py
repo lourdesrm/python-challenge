59e50772990fcf4476c38db68da0fc62d0ace988
Lourdes <lourdes.rm10@gmail.com>
2019-10-25 22:11:19 -0500

    calculate greatest (profit/loss)

--- pybank/main.py
+++ pybank/main.py
@@ -14,6 +14,8 @@ pybank = os.path.join("budget_data.csv")
 months_list=[]
 total_list=[]
 # pybank = open("budget_data.csv")
+print(f"")
+print(f"-------------------------------")
 print(f"Financial Analysis")
 print(f"-------------------------------")
 with open(pybank, "r", newline="") as csvfile:
@@ -48,9 +50,37 @@ total_list_int=list(map(int,total_list))
 totalsum = m.fsum(total_list_int)
 print(f"Total: ${totalsum}")
 
-# calculate average change using numpy module (diff function)
+# calculate ( (n+1) - n ) average change using numpy module (diff function)
 diff_list = np.diff(total_list_int)
 
+# calculate average
+avg_diff = np.average(diff_list)
+
 # print average using numpy module (avg function)
-print(f"Average Change: ${np.average(diff_list)}")
+print(f"Average Change: ${avg_diff}")
+
+# calculate greatest increase in profits
+max_prof = max(diff_list)
+
+# calculate greatest decrease in losses
+max_loss = min(diff_list)
+
+# to get the index associated with the max_prof and max_loss convert array
+# into a list and get the index. Make sure you add 1 to account for the differences
+# in length
+
+list_conv = list(diff_list)
+
+# index calculation for profits
+i = list_conv.index(max_prof) + 1
+
+# index calculation for losses
+j = list_conv.index(max_loss) +1
+#index_profit = np.where(diff_list == max_prof)
+
+print(f"Greatest Increase in Profits: {months_list[i]} (${max_prof})")
+print(f"Greatest Decrease in Profits: {months_list[j]} (${max_loss})")
+
+
+
 
